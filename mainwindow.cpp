3f23b8556a908141eaa21fa2391df7a73b8394b4
FireCruiser <slayer13@comcast.net>
2013-01-27 09:02:52 -0500

    added message box

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QtGui>
+#include <QtCore>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QMessageBox::information(this,"","");
 }
 
 MainWindow::~MainWindow()
